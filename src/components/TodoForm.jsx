9b97b270f84ceaabe44f09445d7e33b383322c5e
Caroli-ne <karoline_gnn@hotmail.com>
2023-10-12 15:20:28 -0300

    feat: funcionalidade para disparar evento ao clicar

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import {useState} from 'react'
 
-const Todoform = () => {
+const Todoform = ({ addTodo }) => {
+    const [value, setValue] = useState("");
+    const [category, setCategory] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(!value || !category) return;
+        addTodo(value,category);
+        setValue("");
+        setCategory("");
+    }
+
 return ( <div className='todoForm'>
     <h2>Nova tarefa:</h2>
-    <form>
-        <input type='text' placeholder='Insira uma tarefa'></input>
-        <select>
+    <form onSubmit={handleSubmit}>
+        <input type='text' placeholder='Insira uma tarefa' value={value} onChange={(e) => setValue(e.target.value) }></input>
+        <select value={category} onChange={(e) => setCategory(e.target.value)}>
             <option value="" >Selecione o tipo de tarefa</option>
             <option value="Urgente">Urgente</option>
             <option value="Importante">Importante</option>
