9cbcae31a3fb9fe5b91f1360bade36c8eafc9985
Caroli-ne <karoline_gnn@hotmail.com>
2023-10-12 15:21:23 -0300

    feat: adiciona o evento excluir e completar ao clique

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 
-const Todo = ({todo}) => {
+const Todo = ({todo, deleteTodo, completeTodo}) => {
 return (
-<div className="todo">
+<div className="todo" style={{textDecoration: todo.completed ? "line-through": ""}}>
     <div className="content">
         <p>{todo.text}</p>
         <p className="category">{todo.category}</p>
     </div>
     <div>
-        <button className='complete'>Completar</button>
+        <button className='complete' onClick={() => completeTodo(todo.id)}>Completar</button>
         <button className='edit'>Editar</button>
-        <button className='exclude'>X</button>
+        <button className='delete' onClick={() => deleteTodo(todo.id)}>X</button>
     </div>
 </div>
 )
