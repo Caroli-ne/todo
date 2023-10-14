e61b635548ae63f330130a44f3929ca82cbc2ea4
Caroli-ne <karoline_gnn@hotmail.com>
2023-10-14 11:16:27 -0300

    fix:preenchimento com dados já inseridos

--- src/components/EditTodoForm.jsx
+++ src/components/EditTodoForm.jsx
@@ -1,8 +1,8 @@
 import {useState} from 'react'
 
 const EditTodoForm = ({ editTodo, todo }) => {
-    const [value, setValue] = useState (todo.todo)
-    const [category, setCategory] = useState (todo.todo)
+    const [value, setValue] = useState (todo.text)
+    const [category, setCategory] = useState (todo.category)
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -15,14 +15,14 @@ const EditTodoForm = ({ editTodo, todo }) => {
 return ( <div className='todoForm'>
     <h2>Editar tarefa:</h2>
     <form onSubmit={handleSubmit}>
-        <input type='text' placeholder='Insira uma tarefa' value={value} onChange={(e) => setValue(e.target.value) }></input>
+        <input type='text' placeholder='Insira uma nova tarefa' value={value} onChange={(e) => setValue(e.target.value) }></input>
         <select value={category} onChange={(e) => setCategory(e.target.value)}>
             <option value="" >Selecione o tipo de tarefa</option>
             <option value="Urgente">Urgente</option>
             <option value="Importante">Importante</option>
             <option value="Circunstancial">Circunstancial</option>
         </select>
-        <button type='submit'>Atualizar tarefa</button>
+        <button type='submit'>Atualizar tarefa</button>     
     </form>
     </div>
 )
