0b6ccb62fbf238c00f2b1e5e1d79eb15f87b0431
Caroli-ne <karoline_gnn@hotmail.com>
2023-10-13 21:52:34 -0300

    feat: configura botão complete

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons"
+
 
 const Todo = ({todo, deleteTodo, completeTodo, editTodo}) => {
 return (
@@ -12,7 +14,7 @@ return (
         <p className="category">{todo.category}</p>
     </div>
     <div>
-        <button className='complete' onClick={() => completeTodo(todo.id)}>Completar</button>
+        <FontAwesomeIcon icon={faCheckCircle} color="#6DB65B" className='complete' onClick={() => completeTodo(todo.id)}/>
         <FontAwesomeIcon className="edit" icon={faPenToSquare} onClick={() => editTodo(todo.id)} />
         <FontAwesomeIcon className="delete" icon={faTrash} onClick={() => deleteTodo(todo.id)} />
     </div>
